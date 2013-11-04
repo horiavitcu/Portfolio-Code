f6d164f8a938c71251fdf328ba3413428fd6c84d
Horia Vitcu <vhoria1@hotmail.com>
2013-11-04 10:34:17 +0000

    Update Description

--- Java/Description_Grabber_Selenium_JUnit/src/base/ClassOne.java
+++ Java/Description_Grabber_Selenium_JUnit/src/base/ClassOne.java
@@ -1,5 +1,9 @@
 package base;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -10,12 +14,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class ClassOne {
 	
 	private static WebDriver firefoxDriver;
-	
+	//private String FILE_LOCATION_NAME = "/Users/horiavitcu/Desktop/desc.txt";
+	private File file;
 	private WebElement element;
 	private List<WebElement> elements;
 	
 	public ClassOne() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setFileLocation(File file) {
+		this.file = file;
 	}
 	
 	public void openSite(String siteURL) {
@@ -39,37 +48,54 @@ public class ClassOne {
 			element = firefoxDriver.findElement(By.tagName(elementAttr));
 		}	
 	}
+	
 	public void findElementXpath(String elementName, String elementAttr, String elementAttrValue) {
 		element = firefoxDriver.findElement(By.xpath("//" + elementName + "[@" + elementAttr + " = " + 
 				 "'" + elementAttrValue + "'" +"]"));
 	}
+	
 	public void typeInSearch(String searchQuery) {
 		
 		element.sendKeys(searchQuery);
 		element.submit();
 	}
 
-	public void storeElements(String elementName, String elementAttr, String elementAttrValue) {
+	public List<WebElement> readElements(String elementName, String elementAttr, String elementAttrValue) {
 		
 		elements = firefoxDriver.findElements(By.xpath("//" + elementName + "[@" + elementAttr + " = " + 
 				 "'" + elementAttrValue + "'" +"]"));
 		for (int i = 0; i < elements.size(); i++) {
 			System.out.println(elements.get(i).getText());
 		}
+		return elements;
+		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void writeFile(List<WebElement> arrayToBeWritten) {
+		 
+		   BufferedWriter writer = null;
+		    try {
+
+		        writer = new BufferedWriter(new FileWriter(file));
+		        for ( int i = 0; i < arrayToBeWritten.size(); i++)
+		        {      
+		          writer.write(arrayToBeWritten.get(i).getText());
+		          writer.newLine();
+		      writer.flush();
+		        }
+
+		    } catch(IOException ex) {
+		        ex.printStackTrace();
+		    } finally{
+		        if(writer!=null){
+		            try {
+						writer.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        }  
+		    }
+	}
+
 }
